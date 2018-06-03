7937cd098b87f1a33eda1c32d0ea9a11127c589b
AlexOuttaspace <matanforeveryone@gmail.com>
2018-06-04 00:29:07 +0300

    add login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {secretOrKey} = require('../../config/keys');
+const passport = require('passport');
 
 // Load user model
 const User = require('../../models/User');
@@ -53,5 +56,55 @@ router.post('/register', (req, res) => {
     });
 });
 
+
+// @route   GET api/users/login
+// @desc    Login user / Returning JWT
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // find user by email
+  User.findOne({email})
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({email: 'User not found'});
+      }
+
+      // Check Password
+      return bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch) {
+            // JWT payload
+            const payload = {
+              id: user.id,
+              name: user.name,
+              avatar: user.avatar
+            }
+    
+            jwt.sign(payload, secretOrKey, {expiresIn: 3600}, (err, token) => {
+              res.json({
+                success: true,
+                token: 'Bearer ' + token
+              });
+            });
+          } else {
+            return res.status(400).json({password: 'Incorrect password'});
+        }
+      });
+    });
+});
+
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email 
+  });
+});
 
 module.exports = router;
