87da64de7963e1d7f20081cb5d8216c4af4b23f3
AlexOuttaspace <matanforeveryone@gmail.com>
2018-06-04 01:20:52 +0300

    add server-side auth validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,10 @@ const jwt = require('jsonwebtoken');
 const {secretOrKey} = require('../../config/keys');
 const passport = require('passport');
 
+// Load input validation
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput    = require('../../validation/login');
+
 // Load user model
 const User = require('../../models/User');
 
@@ -22,10 +26,17 @@ router.get('/test', (req, res) => {
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
+  const {errors, isValid} = validateRegisterInput(req.body);
+
+  if(!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({email: req.body.email})
     .then(user => {
       if(user) {
-        return res.status(400).json({email: 'Email already exists'})
+        errors.email = 'Email already exists';
+        return res.status(400).json(errors);
       } else {
         const avatar = gravatar.url(req.body.email, {
           s: '200',
@@ -61,6 +72,12 @@ router.post('/register', (req, res) => {
 // @desc    Login user / Returning JWT
 // @access  Public
 router.post('/login', (req, res) => {
+  const {errors, isValid} = validateLoginInput(req.body);
+
+  if(!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -68,7 +85,8 @@ router.post('/login', (req, res) => {
   User.findOne({email})
     .then(user => {
       if (!user) {
-        return res.status(404).json({email: 'User not found'});
+        errors.email = 'User not found'
+        return res.status(404).json(errors);
       }
 
       // Check Password
